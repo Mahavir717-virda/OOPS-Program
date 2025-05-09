6ac1a1b491b825121aba4ebfffefa82c54aea02d
Mahavir717-virda <mahavirce7@gmail.com>
2025-05-09 12:06:29 +0530

    Add files via upload

--- Practical_7_5.cpp
+++ Practical_7_5.cpp
@@ -1,26 +1,31 @@
 #include <iostream>
 #include <iomanip>
 #include <string>
+
 using namespace std;
+
+ostream &currency(ostream &out)
+{
+    out << "$";
+    return out;
+}
 
 int main()
 {
-    // Sample data (can be replaced with file input if needed)
+
     string names[] = {"Mahavir", "Deep", "Khushal"};
     int marks[] = {88, 75, 93};
-    float fees[] = {50000.0, 47500.5, 52000.70};
+    float fees[] = {50000.0, 47500.5, 52000.7005};
 
-    // Display header
     cout << setw(15) << "Name"
          << setw(10) << "Marks"
          << setw(15) << "Tuition Fee" << endl;
 
-    // Display data
     for (int i = 0; i < 3; i++)
     {
         cout << setw(15) << names[i]
              << setw(10) << marks[i]
-             << setw(15) << fixed << setprecision(2) << fees[i] << endl;
+             << setw(5) << currency << fixed << setprecision(2) << fees[i] << endl;
     }
 
     return 0;
